b226c1dbe76d5b91a552249089b0d6ddd060859b
IvanJelenic97 <ivan.jelenicw@gmail.com>
2023-05-11 22:47:46 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./login/Login";
+import NewPassword from "./newpassword/NewPassword";
+import Reset from "./reset/Reset";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/newpassword" element={<NewPassword />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
